65ade3c21c67ef1dd540ba75b8cb157ee15dca8b
Insanmln06 <insanmaulana.4483@gmail.com>
2021-12-11 10:59:10 +0700

    BelajarFragment

---
+++ app/src/main/java/com/insanmaulana/belajarfragment/FragmentSatu.kt
@@ -0,0 +1,33 @@
+package com.insanmaulana.belajarfragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_satu.*
+
+class FragmentSatu : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_satu, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val fragmentDua = FragmentDua()
+        btn_dua.setOnClickListener{
+            fragmentManager?.beginTransaction()?.apply {
+                replace(R.id.container, fragmentDua, FragmentDua::class.java.simpleName)
+                    .addToBackStack(null)
+                    .commit()
+            }
+        }
+    }
+
+
+}
